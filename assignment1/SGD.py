7694740c795784fd50eb73cb4dff6a42c9ec4d70
Elad Feldman <eladfeld@post.bgu.ac.il>
2021-12-09 15:55:17 +0200

    gradient test

--- assignment1/SGD.py
+++ assignment1/SGD.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+from DataLoader import DataLoader
 
 def linear_function(x):
     return 6 * x + 3
@@ -24,7 +24,7 @@ def mse_grad_wrb(preds, reals):
     return 2 * np.sum(preds - reals) / len(preds)
 
 
-def sgd(learning_rate, batch_size):
+def sgd_linear(learning_rate, batch_size):
     X = np.array([x for x in np.arange(0, 2, 0.0001)])
     np.random.shuffle(X)
     Y = np.array([noisy_linear_function(x) for x in X])
@@ -37,14 +37,82 @@ def sgd(learning_rate, batch_size):
         preds = f(inputs)
         error = mse(preds, reals)
         print(error)
-        print("mse_grad_wrm", mse_grad_wrm(preds, reals, inputs))
         m -= learning_rate * mse_grad_wrm(preds, reals, inputs)
         b -= learning_rate * mse_grad_wrb(preds, reals)
-        print(f'm: {m}, b:{b}')
     plt.plot(X, Y, 'ro')
     plt.plot([0, 2], [linear_function(0), linear_function(2)])
     plt.plot([0, 2], [f(0), f(2)], '--')
     plt.show()
 
 
-sgd(0.1, 200)
+def softmax(z):
+    e_x = np.exp(z - np.max(z))
+    sum = np.sum(e_x, axis=1, keepdims=True)
+    output = e_x / sum
+    return output
+
+def accuracy(pred, real):
+    (n, m) = pred.shape
+    p = np.argmax(pred, axis=1)
+    r = np.argmax(real, axis=1)
+    n_correct = np.sum(p == r)
+    return n_correct / n
+
+def cross_entropy(pred, real):
+    (n, m) = pred.shape
+    numerate = (np.sum(real * np.log(pred), axis=1))
+    return (numerate / (-m))[0]
+
+
+def ce_grad_wrw(preds, reals, inputs):
+    return np.dot((preds - reals).T, inputs)
+
+
+def ce_grad_wrb(preds, reals):
+    return np.dot((preds - reals).T, (np.ones((preds.shape[0], 1)) / preds.shape[0]))
+
+
+def sgd_softmax(learning_rate, batch_size, epochs, X, Y, X_test, Y_test, sample_num):
+    w = np.random.normal(size=(Y.shape[1], X.shape[1]))
+    b = np.zeros((Y.shape[1], 1))
+    error_train_list, error_test_list, acc_train_list, acc_test_list = [], [], [], []
+    for epoch in range(epochs):
+        for i in range(int(len(X)/ batch_size)):
+            inputs = X[i * batch_size: (i + 1) * batch_size]
+            reals = Y[i * batch_size: (i + 1) * batch_size]
+            z = (np.dot(inputs, w).T + b).T
+            preds = softmax(z)
+            w -= learning_rate * ce_grad_wrw(preds, reals, inputs)
+            b -= learning_rate * ce_grad_wrb(preds, reals)
+        idx_train = np.random.randint(len(X), size=sample_num)
+        idx_test = np.random.randint(len(X_test), size=sample_num)
+        error_train, acc_train, error_test, acc_test = validate(w, b, X[idx_train], Y[idx_train], Y_test[idx_test], Y_test[idx_test])
+        error_train_list += error_train
+        error_test_list += error_train
+        acc_test_list += acc_test
+        acc_train_list += acc_train
+    plot_results(error_train_list, error_test_list, acc_train_list, acc_test_list)
+
+
+def plot_results(error_train_list, error_test_list, acc_train_list, acc_test_list):
+    pass
+
+def validate(w, b, X, Y, X_test, Y_test):
+    z = (np.dot(X, w).T + b).T
+    preds = softmax(z)
+    error = cross_entropy(preds, Y)
+    acc = accuracy(preds, Y)
+    return error_train, acc_train, error_test, acc_test
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+dl = DataLoader('./data/SwissRollData.mat')
+X = dl.training_inputs.T[0: 10000]
+Y = dl.training_labels.T[0: 10000]
+
+# X, Y = unison_shuffled_copies(X, Y)
+# sgd_softmax(0.01, 100, 100, X, Y)
+# sgd_linear(0.1, 200)
--- assignment1/gradientTest.py
+++ assignment1/gradientTest.py
@@ -1,44 +1,40 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from SGD import ce_grad_wrw, ce_grad_wrb, cross_entropy
 
-
-# x.shape = (n, 1)
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / np.sum(e_x, axis=0, keepdims=True)
-
-
-def cross_entropy(pred, real):
-    (n, m) = pred.shape
-    return (np.sum(real * np.log(pred), axis=1) / (-m))[0]
-
-
-def softmax_grad(x):
-    sm = softmax(x)
-    s = sm.reshape(-1, 1)
-    return np.diagflat(s) - np.dot(s, s.T)
-
+def one_hot_vec(size, idx=0):
+    output = np.zeros((size, 1))
+    output[idx] = 1
+    return output
 
 def grad_test_softmax():
-    F = cross_entropy
-    g_F = softmax_grad
     n = 20
-    x = np.random.randn(n)
-    d = np.random.randn(n)
+    x = np.abs(np.random.randn(n, 1))
+    d = np.random.randn(n, 1) / n
     epsilon = 0.1
+    r = one_hot_vec(n)
+
+    F = lambda p: cross_entropy(p, r)
+    g_F = lambda p: ce_grad_wrw(p, r, x)
+
     F0 = F(x)
-    g0 = g_F(x)
+    g0 = g_F(x.T)
     y0 = np.zeros(8)
     y1 = np.zeros(8)
     print("k\terror  oreder 1\t\t error order 2")
     for k in range(8):
         epsk = epsilon * (0.5 ** k)
         Fk = F(x + epsk * d)
-        F1 = F0 + epsk * np.dot(g0, d)
+        F1 = F0 + epsk * np.dot(g0.T, d)
         y0[k] = np.abs(Fk - F0)
         y1[k] = np.abs(Fk - F1)
         print(f'{k} \t {y0[k]} \t {y1[k]}')
+        plt.plot()
     return y0, y1
 
 
-grad_test_softmax()
+y0, y1 = grad_test_softmax()
+plt.plot(y0)
+plt.plot(y1)
+plt.yscale('log')
+plt.show()

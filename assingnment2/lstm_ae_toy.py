5b04c7d4243aa67dba1f0450df7af84f9fc0c557
Elad Feldman <eladfeld@post.bgu.ac.il>
2021-12-20 23:05:22 +0200

    mission one is done

--- assingnment2/lstm_ae_toy.py
+++ assingnment2/lstm_ae_toy.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+
 NUM_SEQUENCES = 10000
 SEQUENCE_SIZE = 50
 LOW = 20
@@ -15,14 +17,19 @@ class data_maker():
             m.append(np.array([(0.1 if j in range(i - 5, i + 6) else 1.0) for j in range(SEQUENCE_SIZE)]))
         m = np.array(m)
         data = data * m
-        return data
+        train_limit = int(NUM_SEQUENCES * 0.8)
+        return torch.Tensor(data[:train_limit]), torch.Tensor(data[train_limit:])
 
-dm = data_maker()
-data = dm.make()
 
-for i in range(3):
-    plt.title(f'synthetic data - example {i + 1}')
+def function_that_prints_the_graphs_but_with_extra_words_in_the_name(data, index, title):
+    plt.title(f'{title} - {index + 1}')
     plt.xlabel('time')
     plt.ylabel('signal')
-    plt.plot(data[i])
+    plt.plot(data[index])
     plt.show()
+
+# dm = data_maker()
+# data = dm.make()
+#
+# for i in range(3):
+#     function_that_prints_the_graphs_but_with_extra_words_in_the_name(i)
